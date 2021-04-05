c32f83d63104397c4707012d561f4d620bdc951f
Yael Eduardo Lopez Tenorio <yael44gr@gmail.com>
2021-04-05 12:09:16 -0500

    Update
    Incluye mejoras al codigo.
    Validacion para a la hora de ver los pokemones (si no exite un pokemon mostrara un mensaje).
    Muestra un spinner en lo que se carga la informacion.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 const url = "https://pokeapi.co/api/v2/";
+let contenido = document.querySelector('#contenido');
 
-getAllPokemones();
+window.onload = () => getAllPokemones();
 
 function getAllPokemones(){
+
+    Spinner();    //Mostramos el spinner
     
-    fetch(`${url}pokemon?limit=100&offset=200`)
+    fetch(`${url}pokemon`)
         .then( resultado => resultado.json() )
         .then( datos => {
 
             console.log(datos);
+
+            limpiarHTML(); //Quitamos el spinner para que se pueda imprimir los datos
 
             const {results } = datos;
 
@@ -16,7 +21,7 @@ function getAllPokemones(){
 
                 const url = pokemon.url.split('/');
 
-                document.querySelector('#contenido').innerHTML += `
+                contenido.innerHTML += `
 
                     <div class="card col-sm-3 p-2 mb-2 ">
 
@@ -51,6 +56,36 @@ function getAllPokemones(){
 
         } );
 
+}
+
+//Funcion para limpiar el HTML
+function limpiarHTML(){
+    while(contenido.firstChild){
+        contenido.removeChild(contenido.firstChild);
+    }
+}
+
+//Funcion para mostrar un spinner
+function Spinner(){
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-circle');
+
+    divSpinner.innerHTML += `
+        <div class="sk-circle1 sk-child"></div>
+        <div class="sk-circle2 sk-child"></div>
+        <div class="sk-circle3 sk-child"></div>
+        <div class="sk-circle4 sk-child"></div>
+        <div class="sk-circle5 sk-child"></div>
+        <div class="sk-circle6 sk-child"></div>
+        <div class="sk-circle7 sk-child"></div>
+        <div class="sk-circle8 sk-child"></div>
+        <div class="sk-circle9 sk-child"></div>
+        <div class="sk-circle10 sk-child"></div>
+        <div class="sk-circle11 sk-child"></div>
+        <div class="sk-circle12 sk-child"></div>
+    `;
+
+    contenido.appendChild(divSpinner);
 }
 
 
--- js/seePokemon.js
+++ js/seePokemon.js
@@ -4,32 +4,76 @@ const id = valores.slice(1, 2);
 
 const url = "https://pokeapi.co/api/v2/";
 
-getPokemon(id)
+let datos = document.querySelector('#datos');
+
+window.onload = () => getPokemon(id);
 
 function getPokemon(id){
 
-    fetch(`${url}pokemon/${id}`)
-        .then( resultado => resultado.json() )
-        .then( datos => {
+    if(id){
 
-            console.log(datos)
+        Spinner();
 
-            document.querySelector('#name').textContent = datos.name;
+        fetch(`${url}pokemon/${id}`)
+            .then( resultado => resultado.json() )
+            .then( datos => {
 
-            document.querySelector('#imagen').innerHTML = `
-                <img src="${datos.sprites.other.dream_world.front_default}" width="250px">
-            `;
+                document.querySelector('.sk-circle').remove();
 
-            datos.moves.forEach( moves => {
-                const { move } = moves;
-                document.querySelector('#descripcion').textContent += move.name + ", ";
-            } )
+                document.querySelector('#name').textContent = datos.name;
 
-            document.querySelector('#exp').textContent = datos.base_experience;
+                document.querySelector('#imagen').innerHTML = `
+                    <img src="${datos.sprites.other.dream_world.front_default}" width="250px">
+                `;
 
-        })
+                datos.moves.forEach( moves => {
+                    const { move } = moves;
+                    document.querySelector('#descripcion').textContent += move.name + ", ";
+                } )
+
+                document.querySelector('#exp').textContent = datos.base_experience;
+
+            });
+    }else{
+        const mensaje = document.createElement('h1');
+        mensaje.classList.add('text-center', 'text-info', 'text-uppercase');
+        mensaje.textContent = "No se encontro el pokemon";
+
+        datos.appendChild(mensaje);
+
+        document.querySelector('#descripcion').remove();
+        document.querySelector('.card-body').remove();
+        document.querySelector('.col-sm-6').remove();
+    }
 
 }
 
+function Spinner(){
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-circle');
+    
+    divSpinner.innerHTML += `
+        <div class="sk-circle1 sk-child"></div>
+        <div class="sk-circle2 sk-child"></div>
+        <div class="sk-circle3 sk-child"></div>
+        <div class="sk-circle4 sk-child"></div>
+        <div class="sk-circle5 sk-child"></div>
+        <div class="sk-circle6 sk-child"></div>
+        <div class="sk-circle7 sk-child"></div>
+        <div class="sk-circle8 sk-child"></div>
+        <div class="sk-circle9 sk-child"></div>
+        <div class="sk-circle10 sk-child"></div>
+        <div class="sk-circle11 sk-child"></div>
+        <div class="sk-circle12 sk-child"></div>
+    `;
+
+    datos.appendChild(divSpinner);
+}
+
+function limpiarHTML(){
+    while(datos.firstChild){
+        datos.removeChild(datos.firstChild);
+    }
+}
 
 
